f0a1c25e5527829d0db38efd9e50d781483af41e
root <root@LAPTOP-H6L4L38B.localdomain>
2020-10-05 22:45:53 +0200

    Init stemmer

---
+++ tests/stemmer.tests.js
@@ -0,0 +1,16 @@
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import { getStem } from '../lib/stemmer'
+
+describe('stemmer', function () {
+  it('always returns stem', function () {
+      const expected = "rdzen";
+      const actual = getStem("test");
+      console.log(expected);
+      console.log(actual);
+      console.log(actual == expected);
+
+      expect(expected).to.equal(actual);
+  })
+
+})
